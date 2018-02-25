c8c0f49a9ec1d856ab56ede5f14363144d15663c
Saravanabalagi R <saravanabalagi@hotmail.com>
2018-02-25 00:59:15 +0000

    Training while playing has errors to fix

--- scripts/capture_data.py
+++ scripts/capture_data.py
@@ -15,6 +15,8 @@ number_of_classes = 9
 
 PATH_IMG = os.path.join(os.getcwd(), '../data/source')
 PATH_KEY = os.path.join(os.getcwd(), '../data/labels.txt')
+PATH_MODELS = os.path.join(os.getcwd(), '../models')
+
 if not os.path.exists(PATH_IMG): os.makedirs(PATH_IMG)
 
 def train(model, screen, label):
@@ -33,6 +35,25 @@ def save_image(img, number, path):
 def save_key(label, file):
 	print(label, file=file)
 
+def get_last_file_number(modeL_name, path):
+    numbers = [-1]
+    for file in os.listdir(path):
+        filename = os.path.splitext(file)[0]
+        ext = os.path.splitext(file)[1]
+        if ext.lower() not in ['.h5']: continue
+        if filename.startswith(model_name): 
+            try: numbers.append(int(filename[len(model_name)+1:]))
+            except: pass
+    counter = max(numbers)
+    return counter
+
+def save_model(model, model_name, model_path):
+	save_count = get_last_file_number(model_name, model_path)+1
+	print("Saving", model_name+ '_{0:03d}'.format(save_count) +'.h5', end='\r')
+	model.save(model_path + '\\' + model_name + '_{0:03d}'.format(save_count) +'.h5')
+	print(model_name+ '_{0:03d}'.format(save_count) +'.h5', "saved successfully")
+	save_count += 1
+
 
 # Wait for 5 seconds before capture
 wait_time = 5
@@ -46,11 +67,14 @@ if(len(sys.argv)==1):
 	print("No training will take place.\nMode: Capture, Save\n\n")
 	training_enabled = False
 else:
-	file_number = int(sys.argv[1])
-	path_models = os.path.join(os.getcwd(), '../models')
-	model_name = 'model_' + '{0:03d}'.format(file_number)
+	model_number = int(sys.argv[1])
+	if(len(sys.argv)==3):
+		model_number_ext = int(sys.argv[2])
+		model_name = 'model_' + '{0:03d}'.format(model_number) + '{0:03d}'.format(model_number_ext)
+	else: model_name = 'model_' + '{0:03d}'.format(model_number)
+	model_name_without_ext = 'model_' + '{0:03d}'.format(model_number)
 	print("Loading", model_name, end='\r')
-	model = load_model(path_models + '\\' + model_name + '.h5')
+	model = load_model(PATH_MODELS + '\\' + model_name + '.h5')
 	print(model_name, "loaded successfully")
 	print("Mode: Capture, Train, Save\n\nCapture Stats:")
 	training_enabled = True
@@ -98,5 +122,4 @@ while True:
 		counter += 1
 
 labels_file.close()
-print("Files successfully saved :)")
-
+save_model(model, model_name_without_ext, PATH_MODELS)
