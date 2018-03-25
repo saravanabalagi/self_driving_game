0aaa7f86dfc872efb6b183f7846c339bacceff8e
Saravanabalagi R <saravanabalagi@hotmail.com>
2018-03-25 15:51:24 +0100

    carrying out experiments

--- experiments/comparisons/activations_and_lrs/model.py
+++ experiments/comparisons/activations_and_lrs/model.py
@@ -7,9 +7,20 @@ from keras.utils import np_utils
 from keras.optimizers import Adam
 from keras.metrics import mae
 from keras.initializers import RandomNormal
+from keras.engine.topology import Layer
+import numpy as np
 
 from keras import backend
 backend.set_image_dim_ordering('tf')
+
+class ScaleLayer(Layer):
+    def __init__(self, output_dim, **kwargs):
+        self.output_dim = output_dim
+        super(ScaleLayer, self).__init__(**kwargs)
+    def call(self, x):
+        return (x-0.5)*2
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_dim)
 
 def loss(y_true, y_pred):
 
@@ -77,6 +88,38 @@ def relu_model(channels=1, learning_rate=0.001):
     dense_4_3 = Dense(128, kernel_initializer='normal', activation='relu', name='dense_4_3')(dense_4_2)
     output = Dense(1, kernel_initializer='normal', activation='tanh', name='output')(dense_4_3)
 
+    model = Model(inputs=image, outputs=output)
+    model.compile(loss=loss, optimizer=Adam(learning_rate), metrics=['mae',accuracy])
+    return model
+
+def relu_with_scaled_sigmoid_model(channels=1, learning_rate=0.001):
+    
+    h = 75          # height of the image
+    w = 100         # width of the image
+    c = channels    # no of channels
+
+    image = Input(shape=(h,w,c))      
+    conv_1_1 = Conv2D(32, (3, 3), kernel_initializer='normal', activation='relu', input_shape=(h, w, c), name='conv_1_1')(image)
+    conv_1_2 = Conv2D(32, (3, 3), kernel_initializer='normal', activation='relu', name='conv_1_2')(conv_1_1)
+    pool_1_2 = MaxPooling2D(pool_size=(2,2), name='pool_1_2')(conv_1_2)
+
+    conv_2_1 = Conv2D(64, (3, 3), kernel_initializer='normal', activation='relu', name='conv_2_1')(pool_1_2)
+    conv_2_2 = Conv2D(64, (3, 3), kernel_initializer='normal', activation='relu', name='conv_2_2')(conv_2_1)
+    conv_2_3 = Conv2D(64, (3, 3), kernel_initializer='normal', activation='relu', name='conv_2_3')(conv_2_2)
+    pool_2_3 = MaxPooling2D(pool_size=(2,2), name='pool_2_3')(conv_2_3)
+
+    conv_3_1 = Conv2D(128, (3, 3), kernel_initializer='normal', activation='relu', name='conv_3_1')(pool_2_3)
+    conv_3_2 = Conv2D(128, (3, 3), kernel_initializer='normal', activation='relu', name='conv_3_2')(conv_3_1)
+    conv_3_3 = Conv2D(128, (3, 3), kernel_initializer='normal', activation='relu', name='conv_3_3')(conv_3_2)
+    pool_3_3 = MaxPooling2D(pool_size=(2,2), name='pool_3_3')(conv_3_3)
+    flatten_3_3 = Flatten(name='flatten_3_3')(pool_3_3)
+
+    dense_4_1 = Dense(2048, kernel_initializer='normal', activation='relu', name='dense_4_1')(flatten_3_3)
+    dense_4_2 = Dense(1024, kernel_initializer='normal', activation='relu', name='dense_4_2')(dense_4_1)
+    dense_4_3 = Dense(128, kernel_initializer='normal', activation='relu', name='dense_4_3')(dense_4_2)
+    dense_4_4 = Dense(1, kernel_initializer='normal', activation='sigmoid', name='dense_4_4')(dense_4_3)
+    output = ScaleLayer(1, name='output')(dense_4_4)
+    
     model = Model(inputs=image, outputs=output)
     model.compile(loss=loss, optimizer=Adam(learning_rate), metrics=['mae',accuracy])
     return model
--- experiments/comparisons/activations_and_lrs/train.py
+++ experiments/comparisons/activations_and_lrs/train.py
@@ -21,11 +21,11 @@ if __name__ == '__main__':
 
     # Get test and train data
     file = "E:\Backup\GTA V Dataset\dataset.pz"
-    x_train, x_test, y_train, y_test = get_test_train_data(file, 100, tanh=False)
+    x_train, x_test, y_train, y_test = get_test_train_data(file, 40000, tanh=False)
     # x_train, x_test, y_train, y_test = get_test_train_data(file, 1000, tanh=False)
 
     learning_rates = [0.01, 0.001, 0.0005, 0.0003, 0.0001]
-    models = ['relu', 'tanh']
+    models = ['tanh', 'relu', 'relu_with_scaled_sigmoid']
 
     for i in range(len(models)):
         for j in range(len(learning_rates)):
@@ -40,6 +40,7 @@ if __name__ == '__main__':
 
 
             if models[i] == 'relu': model = relu_model(learning_rate=learning_rates[j])
+            elif models[i] == 'relu_with_scaled_sigmoid': model = relu_with_scaled_sigmoid_model(learning_rate=learning_rates[j])
             elif models[i] == 'tanh': 
                 model = tanh_model(learning_rate=learning_rates[j])
                 x_train = (x_train - 0.5) * 2 
@@ -50,7 +51,7 @@ if __name__ == '__main__':
 
             # Train model
             tbCallBack = TensorBoard(log_dir=outfolder, histogram_freq=0, write_graph=True, write_images=True)
-            model.fit(x_train, y_train, validation_split=0.2, epochs=10, batch_size=250, callbacks=[tbCallBack])
+            model.fit(x_train, y_train, validation_split=0.2, epochs=25, batch_size=250, callbacks=[tbCallBack])
             save(model, path=outfolder)
 
             # Evaluate model
