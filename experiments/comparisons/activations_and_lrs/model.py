449bf27a2f98db4d7365b5cc97ab0f3c16f95c73
Saravanabalagi R <saravanabalagi@hotmail.com>
2018-03-25 00:41:39 +0000

    Cleaning up before experiments activations and lrs

--- experiments/comparisons/activations_and_lrs/model.py
+++ experiments/comparisons/activations_and_lrs/model.py
@@ -11,13 +11,15 @@ from keras.initializers import RandomNormal
 from keras import backend
 backend.set_image_dim_ordering('tf')
 
-def loss(y_true, y_pred): return backend.sum(backend.abs(y_true - y_pred))
+def loss(y_true, y_pred):
+
+    return backend.sum(backend.abs(y_true - y_pred))
 def accuracy(y_true, y_pred): return 1 - backend.mean(backend.minimum(1.0,((backend.abs(y_true - y_pred))*10)))
 
 loss.__name__ == 'Sum of Absolute Errors'
 accuracy.__name__ == 'Mean of Absolute Errors of upto 0.1'
 
-def tanh_model(channels=1):
+def tanh_model(channels=1, learning_rate=0.001):
     
     h = 75          # height of the image
     w = 100         # width of the image
@@ -42,39 +44,39 @@ def tanh_model(channels=1):
     dense_4_1 = Dense(2048, kernel_initializer='normal', activation='tanh', name='dense_4_1')(flatten_3_3)
     dense_4_2 = Dense(1024, kernel_initializer='normal', activation='tanh', name='dense_4_2')(dense_4_1)
     dense_4_3 = Dense(128, kernel_initializer='normal', activation='tanh', name='dense_4_3')(dense_4_2)
-    output = Dense(1)(dense_4_3)
+    output = Dense(1, kernel_initializer='normal', activation='tanh', name='output')(dense_4_3)
 
     model = Model(inputs=image, outputs=output)
-    model.compile(loss=loss, optimizer=Adam(0.001), metrics=['mae',accuracy])
+    model.compile(loss=loss, optimizer=Adam(learning_rate), metrics=['mae',accuracy])
     return model
 
-def relu_model(channels=1):
+def relu_model(channels=1, learning_rate=0.001):
     
     h = 75          # height of the image
     w = 100         # width of the image
     c = channels    # no of channels
 
-    model = Sequential()
-        
-    model.add(Conv2D(32, (7, 7), kernel_initializer='normal', activation='relu', input_shape=(h, w, c)))
-    model.add(Conv2D(32, (7, 7), kernel_initializer='normal', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    image = Input(shape=(h,w,c))      
+    conv_1_1 = Conv2D(32, (3, 3), kernel_initializer='normal', activation='relu', input_shape=(h, w, c), name='conv_1_1')(image)
+    conv_1_2 = Conv2D(32, (3, 3), kernel_initializer='normal', activation='relu', name='conv_1_2')(conv_1_1)
+    pool_1_2 = MaxPooling2D(pool_size=(2,2), name='pool_1_2')(conv_1_2)
 
-    model.add(Conv2D(64, (5, 5), kernel_initializer='normal', activation='relu'))
-    model.add(Conv2D(64, (5, 5), kernel_initializer='normal', activation='relu'))
-    model.add(Conv2D(64, (5, 5), kernel_initializer='normal', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    conv_2_1 = Conv2D(64, (3, 3), kernel_initializer='normal', activation='relu', name='conv_2_1')(pool_1_2)
+    conv_2_2 = Conv2D(64, (3, 3), kernel_initializer='normal', activation='relu', name='conv_2_2')(conv_2_1)
+    conv_2_3 = Conv2D(64, (3, 3), kernel_initializer='normal', activation='relu', name='conv_2_3')(conv_2_2)
+    pool_2_3 = MaxPooling2D(pool_size=(2,2), name='pool_2_3')(conv_2_3)
 
-    model.add(Conv2D(128, (3, 3), kernel_initializer='normal', activation='relu'))
-    model.add(Conv2D(128, (3, 3), kernel_initializer='normal', activation='relu'))
-    model.add(Conv2D(128, (3, 3), kernel_initializer='normal', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    conv_3_1 = Conv2D(128, (3, 3), kernel_initializer='normal', activation='relu', name='conv_3_1')(pool_2_3)
+    conv_3_2 = Conv2D(128, (3, 3), kernel_initializer='normal', activation='relu', name='conv_3_2')(conv_3_1)
+    conv_3_3 = Conv2D(128, (3, 3), kernel_initializer='normal', activation='relu', name='conv_3_3')(conv_3_2)
+    pool_3_3 = MaxPooling2D(pool_size=(2,2), name='pool_3_3')(conv_3_3)
+    flatten_3_3 = Flatten(name='flatten_3_3')(pool_3_3)
 
-    model.add(Flatten())
-    model.add(Dense(2048, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1024, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(128, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1))
+    dense_4_1 = Dense(2048, kernel_initializer='normal', activation='relu', name='dense_4_1')(flatten_3_3)
+    dense_4_2 = Dense(1024, kernel_initializer='normal', activation='relu', name='dense_4_2')(dense_4_1)
+    dense_4_3 = Dense(128, kernel_initializer='normal', activation='relu', name='dense_4_3')(dense_4_2)
+    output = Dense(1, kernel_initializer='normal', activation='tanh', name='output')(dense_4_3)
 
-    model.compile(loss=loss, optimizer=Adam(0.001), metrics=['mae',accuracy])
+    model = Model(inputs=image, outputs=output)
+    model.compile(loss=loss, optimizer=Adam(learning_rate), metrics=['mae',accuracy])
     return model
---
+++ experiments/comparisons/activations_and_lrs/train.py
@@ -0,0 +1,81 @@
+from keras.callbacks import TensorBoard
+from keras import backend
+from model import relu_model, tanh_model
+from test import evaluate_model
+from save_load import save, get_last_file_number
+from preprocess import get_test_train_data
+from utils import print_model_summary
+import tensorflow as tf
+import sys, os
+
+if __name__ == '__main__':
+
+    # # Print output to file
+    # outfolder = 'exp_' + '{0:03d}'.format(get_last_file_number(prefix='exp_', suffix='') + 1); os.makedirs(outfolder)
+    # outfile = outfolder + '/' + 'train_' + '{0:03d}'.format(get_last_file_number(path=outfolder) + 1) + '.log'
+    # print('Printing to logfile at', outfile)
+    # sys.stdout = open(outfile, 'w+')
+
+    # Add title to logfile for identification
+    # if len(sys.argv)>1: print('Title:',sys.argv[1],'\n\n')
+
+    # Get test and train data
+    file = "E:\Backup\GTA V Dataset\dataset.pz"
+    x_train, x_test, y_train, y_test = get_test_train_data(file, 100, tanh=False)
+    # x_train, x_test, y_train, y_test = get_test_train_data(file, 1000, tanh=False)
+
+    learning_rates = [0.01, 0.001, 0.0005, 0.0003, 0.0001]
+    models = ['relu', 'tanh']
+
+    for i in range(len(models)):
+        for j in range(len(learning_rates)):
+
+            # Print output to file
+            outfolder = 'exp_' + '{0:03d}'.format(get_last_file_number(prefix='exp_', suffix='') + 1); os.makedirs(outfolder)
+            outfile = outfolder + '/' + 'train_' + '{0:03d}'.format(get_last_file_number(path=outfolder) + 1) + '.log'
+            print('Printing to logfile at', outfile)
+            sys.stdout = open(outfile, 'w+')
+
+            print('Title:', '{}_adam_{}'.format(models[i], learning_rates[j]),'\n\n')
+
+
+            if models[i] == 'relu': model = relu_model(learning_rate=learning_rates[j])
+            elif models[i] == 'tanh': 
+                model = tanh_model(learning_rate=learning_rates[j])
+                x_train = (x_train - 0.5) * 2 
+                x_test = (x_test - 0.5) * 2
+
+            # Print model summary
+            print_model_summary(model)
+
+            # Train model
+            tbCallBack = TensorBoard(log_dir=outfolder, histogram_freq=0, write_graph=True, write_images=True)
+            model.fit(x_train, y_train, validation_split=0.2, epochs=10, batch_size=250, callbacks=[tbCallBack])
+            save(model, path=outfolder)
+
+            # Evaluate model
+            scores = evaluate_model(model, x_test, y_test)
+
+            # Print scores
+            print('\n\n')
+            print("Loss: ", backend.get_value(scores[0]))
+            print("Accuracy: ", backend.get_value(scores[1])*100, "%")
+
+
+    # # Create model and print to log file
+    # model = tanh_model()
+    # # model = relu_model()
+    # print_model_summary(model)
+
+    # # Train model
+    # tbCallBack = TensorBoard(log_dir=outfolder, histogram_freq=0, write_graph=True, write_images=True)
+    # model.fit(x_train, y_train, validation_split=0.2, epochs=10, batch_size=250, callbacks=[tbCallBack])
+    # save(model, path=outfolder)
+
+    # # Evaluate model
+    # scores = evaluate_model(model, x_test, y_test)
+
+    # # Print scores
+    # print('\n\n')
+    # print("Loss: ", backend.get_value(scores[0]))
+    # print("Accuracy: ", backend.get_value(scores[1])*100, "%")
