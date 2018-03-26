7d583fb0db17495664b55d683f50d3f342454ed4
Saravanabalagi R <saravanabalagi@hotmail.com>
2018-03-26 03:13:54 +0100

    Adding ScaleLayer in play_game

--- scripts/play_game.py
+++ scripts/play_game.py
@@ -1,4 +1,5 @@
 from keras.models import load_model
+from keras.models import Layer
 from keras import backend
 from grab_screen import get_scaled_grayscale, get_screen
 from grab_key import get_keys
@@ -8,13 +9,23 @@ import os, sys
 import time
 import cv2
 
+class ScaleLayer(Layer):
+    def __init__(self, **kwargs):
+        super(ScaleLayer, self).__init__(**kwargs)
+    def call(self, x):
+        return (x-0.5)*2
+    def compute_output_shape(self, input_shape):
+        return input_shape
+    def build(self, input_shape):
+        super(ScaleLayer, self).build(input_shape)
+
 def loss(y_true, y_pred): return backend.sum(backend.abs(y_true - y_pred))
 def accuracy(y_true, y_pred): return 1 - backend.mean(backend.minimum(1.0,((backend.abs(y_true - y_pred))*10)))
 
 file_number = int(sys.argv[1])
 path_models = os.path.join(os.getcwd(), '../models')
 model_name = 'model_' + '{0:03d}'.format(file_number)
-model = load_model(path_models + '\\' + model_name + '.h5', custom_objects={'loss': loss, 'accuracy': accuracy})
+model = load_model(path_models + '\\' + model_name + '.h5', custom_objects={'loss': loss, 'accuracy': accuracy, 'ScaleLayer': ScaleLayer})
 
 print("Starting...\nPress O to pause\nPress L to stop")
 
