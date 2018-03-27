acc9b89bf0821e21afd4748ca41a30a90136a017
psambit9791 <sambitpaul1992@gmail.com>
2018-03-27 19:03:33 +0100

    New model for 3 channels

---
+++ experiments/comparisons/filter_visualisation/save_load.py
@@ -0,0 +1,36 @@
+from keras.models import load_model
+from model import loss, accuracy
+import h5py
+import os
+from model import ScaleLayer
+
+def get_last_file_number(path=None, prefix='model_', suffix='.h5'):
+    if path is None: path = os.getcwd()
+    valid_model_files = [suffix]
+    numbers = [-1]
+    for file in os.listdir(path):
+        filename = os.path.splitext(file)[0]
+        ext = os.path.splitext(file)[1]
+        if ext.lower() not in valid_model_files: continue
+        if filename.startswith(prefix): 
+            numbers.append(int(''.join(list(filter(str.isdigit, filename)))))
+    count = max(numbers)
+    return count
+
+def save(model, path=None, count=None):
+    if path is None: path = os.getcwd()
+    if count is None: count = get_last_file_number(path) + 1
+
+    model_name = 'model_' + '{0:03d}'.format(count)
+    model.save(path + '/' + model_name + '.h5')
+
+    print("Saving model:" + model_name + '.h5')
+
+def load(count=None, path=None):
+    if path is None: path = os.getcwd()
+    if count is None: count = get_last_file_number(path)
+    if count == -1: print("File not found"); return
+    model_name = 'model_' + '{0:03d}'.format(count)
+    model = load_model(path + '/' + model_name + '.h5', custom_objects={'loss': loss, 'accuracy': accuracy, 'ScaleLayer':ScaleLayer})
+    print('Loaded model:',path + '/' + model_name + '.h5')
+    return model
---
+++ experiments/comparisons/filter_visualisation/train.py
@@ -0,0 +1,84 @@
+from keras.callbacks import TensorBoard
+from keras import backend
+from model import relu_model, tanh_model, relu_with_scaled_sigmoid_model
+from test import evaluate_model
+from save_load import save, get_last_file_number
+from preprocess import get_test_train_data
+from utils import print_model_summary
+import tensorflow as tf
+import sys, os
+
+if __name__ == '__main__':
+
+    # # Print output to file
+    # outfolder = 'exp_' + '{0:03d}'.format(get_last_file_number(prefix='exp_', suffix='') + 1); os.makedirs(outfolder)
+    # outfile = outfolder + '/' + 'train_' + '{0:03d}'.format(get_last_file_number(path=outfolder) + 1) + '.log'
+    # print('Printing to logfile at', outfile)
+    # sys.stdout = open(outfile, 'w+')
+
+    # Add title to logfile for identification
+    # if len(sys.argv)>1: print('Title:',sys.argv[1],'\n\n')
+
+    # Get test and train data
+    file = os.environ['DATA_DIR']+ "/dataset_75p_gray.pz"
+    x_train, x_test, y_train, y_test = get_test_train_data(file, 80000, tanh=False)
+    # x_train, x_test, y_train, y_test = get_test_train_data(file, 1000, tanh=False)
+
+    learning_rate = 0.0005
+    models = ['relu_with_scaled_sigmoid']
+    drop_rates = [0.1]
+
+    for i in range(len(models)):
+        for j in range(len(drop_rates)):
+
+            # Print output to file
+            outfolder = 'exp_' + '{0:03d}'.format(get_last_file_number(prefix='exp_', suffix='') + 1); os.makedirs(outfolder)
+            outfile = outfolder + '/' + 'train_' + '{0:03d}'.format(get_last_file_number(path=outfolder) + 1) + '.log'
+            print('Printing to logfile at', outfile)
+            sys.stdout = open(outfile, 'w+')
+
+            print('Title:', '{}_adam_{}_dropout_rate_{}'.format(models[i], learning_rate, drop_rates[j]),'\n\n')
+
+
+            if models[i] == 'relu': model = relu_model(learning_rate=learning_rate)
+            elif models[i] == 'relu_with_scaled_sigmoid': model = relu_with_scaled_sigmoid_model(learning_rate=learning_rate, drop_rate=drop_rates[i])
+            elif models[i] == 'tanh': 
+                model = tanh_model(learning_rate=learning_rate, drop_rate=drop_rates[i])
+                x_train = (x_train - 0.5) * 2 
+                x_test = (x_test - 0.5) * 2
+
+            # Print model summary
+            print_model_summary(model)
+
+            # Train model
+            tbCallBack = TensorBoard(log_dir=outfolder, histogram_freq=0, write_graph=True, write_images=True)
+            model.fit(x_train, y_train, validation_split=0.2, epochs=25, batch_size=250, callbacks=[tbCallBack])
+            save(model, path=outfolder)
+
+            # Evaluate model
+            scores = evaluate_model(model, x_test, y_test)
+
+            # Print scores
+            print('\n\n')
+            print("Loss: ", backend.get_value(scores[0]))
+            print("Accuracy: ", backend.get_value(scores[1])*100, "%")
+
+
+    # # Create model and print to log file
+    # model = tanh_model()
+    # # model = relu_model()
+    # print_model_summary(model)
+
+    # # Train model
+    # tbCallBack = TensorBoard(log_dir=outfolder, histogram_freq=0, write_graph=True, write_images=True)
+    # model.fit(x_train, y_train, validation_split=0.2, epochs=10, batch_size=250, callbacks=[tbCallBack])
+    # save(model, path=outfolder)
+
+    # # Evaluate model
+    # scores = evaluate_model(model, x_test, y_test)
+
+    # # Print scores
+    # print('\n\n')
+    # print("Loss: ", backend.get_value(scores[0]))
+    # print("Accuracy: ", backend.get_value(scores[1])*100, "%")
+
