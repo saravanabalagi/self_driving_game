a7f1e39a8f519a5b746aad6ce6d87f8f9c644b15
Sambit Paul <s1769454@escience5.inf.ed.ac.uk>
2018-03-28 19:37:11 +0100

    Experiments completed!!!

---
+++ experiments/regression/frame_map_all/preprocess.py
@@ -0,0 +1,97 @@
+import numpy as np
+import argparse
+import pickle
+import gzip
+import os
+import cv2
+
+
+def get_prev_steer_matrix(pick_last_steering, y):
+    holder = np.zeros((y.shape[0], pick_last_steering))
+    for i in range(holder.shape[0]):
+        count = 0
+        for j in range(i, i-pick_last_steering, -1):
+            holder[i][count] = y[j-pick_last_steering]*(j>0)
+            count += 1
+    holder = np.array(holder)
+    return(holder)
+
+def get_test_train_data(file, data_limit=-1, tanh=False):
+
+    # Set seed
+    np.random.seed(0)
+
+    # Print Frame shape
+    pfile = gzip.open(file, mode='rb')
+    var = pickle.load(pfile)
+    im_shape = var['frame'].shape
+    print('Original Frame shape:', var['frame'].shape)
+
+    # Load all variables
+    count = 0
+    images = []
+    outputs = []
+    minimaps = []
+    speeds = []
+    yawRates = []
+
+    pfile = gzip.open(file, mode='rb')
+    while True:
+        try:
+            # Load var from pickle
+            var = pickle.load(pfile)
+            count += 1
+                
+            # Resize and load image
+            image = var['frame']
+            if(len(image.shape)==2):
+                image = image[..., None]
+            images.append(image)
+            
+            minimap = var['minimap']
+            if(len(minimap.shape)==2):
+                minimap = minimap[..., None]
+            minimaps.append(minimap)
+            
+            speeds.append(var['speed'])
+            yawRates.append(var['yawRate'])
+
+            # Append outputs
+            outputs.append([var['steering']])
+
+            # Stopping criteria
+            if data_limit!=-1 and count>=data_limit: break
+        except Exception: break
+    
+    y = np.array(outputs)
+    images = np.array(images)
+    minimaps = np.array(minimaps)
+    prev_steer = get_prev_steer_matrix(5, y)
+    speeds = np.array(speeds)
+    yawRates = np.array(yawRates)
+    x = [images, minimaps, prev_steer, speeds, yawRates]
+
+    x_shape = [entity.shape for entity in x]
+    print('Dataset Shape: x: {} | y: {}'.format(x_shape, y.shape))
+
+    # Normalize data
+    if not isinstance(x, np.ndarray): 
+    	x_new = [(entity/255) for i, entity in enumerate(x) if i<2]
+    	if len(x)>=2: x_new = [*x_new, *x[2:]]
+    	x = x_new
+    else: x=x/255
+    np.clip(y, -1, 1, out=y)
+
+    # Test train split
+    from sklearn.model_selection import train_test_split
+    *recv,  y_train, y_test = train_test_split(*x, y, test_size=0.2, random_state=np.random, shuffle=True)
+    
+    x_train = recv[::2]
+    x_test = recv[1::2]
+
+    print()
+    print("Train Data | Test Data")
+    print(("{0:^10} | {1:^10}").format(y_train.shape[0], y_test.shape[0]))
+    print()
+
+    return x_train, x_test, y_train, y_test
---
+++ experiments/regression/frame_map_all/train.py
@@ -0,0 +1,58 @@
+from keras.callbacks import TensorBoard
+from keras import backend
+from model import relu_with_scaled_sigmoid_model
+from test import evaluate_model
+from save_load import save, get_last_file_number
+from preprocess import get_test_train_data
+from utils import print_model_summary
+import tensorflow as tf
+import sys, os
+
+if __name__ == '__main__':
+
+    # # Print output to file
+    # outfolder = 'exp_' + '{0:03d}'.format(get_last_file_number(prefix='exp_', suffix='') + 1); os.makedirs(outfolder)
+    # outfile = outfolder + '/' + 'train_' + '{0:03d}'.format(get_last_file_number(path=outfolder) + 1) + '.log'
+    # print('Printing to logfile at', outfile)
+    # sys.stdout = open(outfile, 'w+')
+
+    # Add title to logfile for identification
+    # if len(sys.argv)>1: print('Title:',sys.argv[1],'\n\n')
+
+    # Get test and train data
+    file = os.environ['DATA_DIR']+ "/dataset_75p_gray.pz"
+    x_train, x_test, y_train, y_test = get_test_train_data(file, 80000, tanh=False)
+    # x_train, x_test, y_train, y_test = get_test_train_data(file, 1000, tanh=False)
+
+    learning_rate = 0.0005
+    model = 'relu_with_scaled_sigmoid'
+    drop_rate = 0.1
+    pick_last_steer = 5
+
+    # Print output to file
+    outfolder = 'exp_' + '{0:03d}'.format(get_last_file_number(prefix='exp_', suffix='') + 1); os.makedirs(outfolder)
+    outfile = outfolder + '/' + 'train_' + '{0:03d}'.format(get_last_file_number(path=outfolder) + 1) + '.log'
+    print('Printing to logfile at', outfile)
+    sys.stdout = open(outfile, 'w+')
+    
+    print('Title:', '{}_adam_{}_dropout_rate_{}'.format(model, learning_rate, drop_rate),'\n\n')
+    
+    model = relu_with_scaled_sigmoid_model(learning_rate=learning_rate, drop_rate=drop_rate, pick_last_steer = pick_last_steer)
+    
+    # Print model summary
+    print_model_summary(model)
+    
+    # Train model
+    tbCallBack = TensorBoard(log_dir=outfolder, histogram_freq=0, write_graph=True, write_images=True)
+    model.fit(x_train, y_train, validation_split=0.2, epochs=25, batch_size=250, callbacks=[tbCallBack])
+    save(model, path=outfolder)
+    
+    # Evaluate model
+    scores = evaluate_model(model, x_test, y_test)
+
+    # Print scores
+    print('\n\n')
+    print("Loss: ", backend.get_value(scores[0]))
+    print("Accuracy: ", backend.get_value(scores[1])*100, "%")
+
+
